a789f3ef02449559349ffe6ebdd38865ec818bb3
Aman Kumar <akt9802@gmail.com>
2026-07-13 22:58:51 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // we can't sort this question because index is involved
+        // when we are stading at a particular index we must think that we 
+        // only need to consider previous k elements
+
+        // use set
+        unordered_set<int> st;
+        for(int i=0;i<nums.size();i++){
+            if(st.count(nums[i])){
+                return true;
+            }
+            
+            st.insert(nums[i]);
+            if(st.size()>k){
+                st.erase(nums[i-k]);
+            }
+        }
+        return false;
+    }
+};
