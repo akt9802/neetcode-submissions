fa27d57d25236773c457ef942d4f277b12ab976a
Aman Kumar <akt9802@gmail.com>
2026-09-02 14:06:00 +0530

    Add: contains-duplicate-ii - submission-10

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-10.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        set<int> st;
+        int i = 0;
+        int j = 0;
+        while(j<nums.size()){
+            if(j-i<=k){
+                if(st.find(nums[j]) != st.end()){
+                    // it exist
+                    return true;
+                }else{
+                    st.insert(nums[j]);
+                    j++;
+                }
+            }else{
+                st.erase(nums[i]);
+                i++;
+            }
+        }
+        return false;
+    }
+};
